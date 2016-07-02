24a1734bed74e23f817aaf155c14c1551753b28e
moraisandre <andrewinvista@gmail.com>
2016-07-02 12:10:10 -0300

    Wiggle, wiggle, wiggle Just a little bit of... Rotate!

--- CardGameV2/Card.swift
+++ CardGameV2/Card.swift
@@ -33,6 +33,9 @@ class Card : SKSpriteNode {
     let frontTexture :SKTexture
     let backTexture :SKTexture
     
+    var damage = 0
+    let damageLabel :SKLabelNode
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("NSCoding not supported")
     }
@@ -50,6 +53,35 @@ class Card : SKSpriteNode {
             frontTexture = SKTexture(imageNamed: "card_creature_dragon")
         }
         
+        damageLabel = SKLabelNode(fontNamed: "OpenSans-Bold")
+        damageLabel.name = "damageLabel"
+        damageLabel.fontSize = 12
+        damageLabel.fontColor = SKColor(red: 0.47, green: 0.0, blue: 0.0, alpha: 1.0)
+        damageLabel.text = "0"
+        damageLabel.position = CGPoint(x: 25, y: 40)
+        
         super.init(texture: frontTexture, color: .clearColor(), size: frontTexture.size())
+        
+        addChild(damageLabel)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- CardGameV2/GameScene.swift
+++ CardGameV2/GameScene.swift
@@ -60,7 +60,24 @@ class GameScene: SKScene {
         for touch in touches {
             let location = touch.locationInNode(self)
             if let card = nodeAtPoint(location) as? Card {
+                //Wiggle
+                let wiggleIn = SKAction.scaleXTo(1.0, duration: 0.2)
+                let wiggleOut = SKAction.scaleXTo(1.2, duration: 0.2)
+                let wiggle = SKAction.sequence([wiggleIn, wiggleOut])
+                
+                card.runAction(SKAction.repeatActionForever(wiggle), withKey: "wiggle")
+                
+                //Rotate
+//                let rotR = SKAction.rotateByAngle(0.15, duration: 0.2)
+//                let rotL = SKAction.rotateByAngle(-0.15, duration: 0.2)
+//                let cycle = SKAction.sequence([rotR, rotL, rotL, rotR])
+//                let wiggle = SKAction.repeatActionForever(cycle)
+//                card.runAction(wiggle, withKey: "rotate") //Should be rotate instead "wiggle" like in tutorial link
+                
                 card.zPosition = CardLevel.moving.rawValue
+                card.removeActionForKey("drop")
+                card.runAction(SKAction.scaleTo(1.3, duration: 0.25), withKey: "pickup")
+
             }
         }
     }
@@ -72,8 +89,28 @@ class GameScene: SKScene {
                 card.zPosition = CardLevel.board.rawValue
                 card.removeFromParent()
                 addChild(card)
+                //Wiggle
+                card.removeActionForKey("wiggle")
+                
+                //Rotate
+//                card.runAction(SKAction.rotateToAngle(0, duration: 0.2), withKey:"rotate")
+
+                card.removeActionForKey("pickup")
+                card.runAction(SKAction.scaleTo(1.0, duration: 0.25), withKey: "drop")
             }
         }
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
